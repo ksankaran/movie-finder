96190c3c3ecb634a36d148417f0081e396baa65b
Velu Sankaran <kv.coder@gmail.com>
2025-07-06 04:10:26 -0400

    figured out the issue

--- auth.py
+++ auth.py
@@ -4,10 +4,8 @@ auth = Auth()
 
 @auth.authenticate 
 async def authenticate(headers: dict) -> Auth.types.MinimalUserDict:
-  print(f"Headers: {headers}")
   origin = headers[b'origin'].decode("utf-8")
-  print(origin)
-  if origin != "movie-finder-ae697f7821735cd0abc9dac7c43c5980.us.langgraph.app":
+  if origin != "https://findmymovie.ai":
     raise Auth.exceptions.HTTPException(status_code=401, detail="Invalid Auth Provided")
 
   return { 
